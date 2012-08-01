0cf0f5c8dd024790e5aa3c36d025b33d2f4bca87
Yanis Guenane <yguenane@gmail.com>
2012-08-01 18:03:33 +0200

    Adapting mysql_backend.rb so it can work with the coming hiera-puppet 1.0.0

--- lib/hiera/backend/mysql_backend.rb
+++ lib/hiera/backend/mysql_backend.rb
@@ -12,7 +12,7 @@ class Hiera
                   require 'rubygems'
                   require 'mysql'
                 end
-                
+
                 Hiera.debug("mysql_backend initialized")
             end
             def lookup(key, scope, order_override, resolution_type)
@@ -26,7 +26,15 @@ class Hiera
                 mysql_query = Backend.parse_string(Config[:mysql][:query], scope, { "key" => key })
 
 
-                answer = Backend.empty_answer(resolution_type)
+                case resolution_type
+                  when :array
+                    answer = []
+                  when :hash
+                    answer = {}
+                  else
+                    answer = nil
+                end
+
                 Hiera.debug("resolution type is #{resolution_type}")
 
                 results = query(mysql_query)
